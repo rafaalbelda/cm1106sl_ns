0418f4bef733e4ade367bc0f7a1544a1626aa7ef
rafaflan <rafa.albelda@gmail.com>
2026-04-07 19:51:44 +0200

    fix id

--- esphome/components/cm1106sl_ns/sensor.py
+++ esphome/components/cm1106sl_ns/sensor.py
@@ -2,31 +2,52 @@ import esphome.codegen as cg
 import esphome.config_validation as cv
 from esphome.components import uart, sensor, text_sensor, binary_sensor
 from esphome.const import (
+    CONF_CO2,
     CONF_ID,
     CONF_NAME,
-    UNIT_PARTS_PER_MILLION,
-    ICON_MOLECULE_CO2,
     DEVICE_CLASS_CARBON_DIOXIDE,
+    ICON_MOLECULE_CO2,
+    STATE_CLASS_MEASUREMENT,
+    UNIT_PARTS_PER_MILLION,
 )
 
 DEPENDENCIES = ["uart"]
-AUTO_LOAD = ["sensor", "text_sensor", "binary_sensor"]
+#AUTO_LOAD = ["sensor", "text_sensor", "binary_sensor"]
 
-cm1106sl_ns_ns = cg.global_ns.namespace("cm1106sl_ns")
+#cm1106sl_ns_ns = cg.global_ns.namespace("cm1106sl_ns")
+cm1106sl_ns_ns = cg.esphome_ns.namespace("cm1106sl_ns")
 CM1106SLNS = cm1106sl_ns_ns.class_("CM1106SLNS", cg.PollingComponent, uart.UARTDevice)
 
 CONF_UART_ID = "uart_id"
 
-CONFIG_SCHEMA = cv.Schema(
-    {
-        cv.GenerateID(): cv.declare_id(CM1106SLNS),
-        cv.Required(CONF_UART_ID): cv.use_id(uart.UARTComponent),
-        cv.Required(CONF_NAME): cv.string,
-    }
-).extend(cv.polling_component_schema("5s"))
+CONFIG_SCHEMA = (
+    cv.Schema(
+        {
+            cv.GenerateID(): cv.declare_id(CM1106SLNS),
+            #cv.Required(CONF_UART_ID): cv.use_id(uart.UARTComponent),
+            cv.Required(CONF_NAME): cv.string,
+            cv.Optional(CONF_CO2): sensor.sensor_schema(
+                unit_of_measurement=UNIT_PARTS_PER_MILLION,
+                icon=ICON_MOLECULE_CO2,
+                accuracy_decimals=0,
+                device_class=DEVICE_CLASS_CARBON_DIOXIDE,
+                state_class=STATE_CLASS_MEASUREMENT,
+            ),
+        },
+    ).extend(cv.polling_component_schema("60s"))
+     .extend(uart.UART_DEVICE_SCHEMA)
+)
 
 
-async def to_code(config):
+async def to_code(config) -> None:
+    #var = cg.new_Pvariable(config[CONF_ID], await cg.get_variable(config[CONF_UART_ID]))
+    var = cg.new_Pvariable(config[CONF_ID])
+    await cg.register_component(var, config)
+    await uart.register_uart_device(var, config)
+    if co2_config := config.get(CONF_CO2):
+        sens = await sensor.new_sensor(co2_config)
+        cg.add(var.set_co2_sensor(sens))
+
     # CO2 principal
     co2 = await sensor.new_sensor(
         {
@@ -36,9 +57,6 @@ async def to_code(config):
             "icon": ICON_MOLECULE_CO2,
             "device_class": DEVICE_CLASS_CARBON_DIOXIDE,
         }
-    var = cg.new_Pvariable(config[CONF_ID], await cg.get_variable(config[CONF_UART_ID]))
-    await cg.register_component(var, config)
-    await uart.register_uart_device(var, config)
 
     )
     cg.add(var.co2_sensor, co2)
