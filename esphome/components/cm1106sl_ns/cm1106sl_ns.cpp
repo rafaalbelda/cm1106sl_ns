11a348707be21bae1b3e331a94aacfabe64fb154
rafaflan <rafa.albelda@gmail.com>
2026-04-23 16:28:11 +0200

    fix

--- esphome/components/cm1106sl_ns/cm1106sl_ns.cpp
+++ esphome/components/cm1106sl_ns/cm1106sl_ns.cpp
@@ -462,9 +462,10 @@ bool CM1106SLNSComponent::cm1106_get_measurement_period_(uint16_t *period, uint8
   
   // Validate checksum
   uint8_t expected_cs = this->cm1106_checksum_(response, sizeof(response));
+  
   if (response[6] != expected_cs) {
     ESP_LOGW(TAG, "Measurement period response checksum mismatch: expected 0x%02X, got 0x%02X",
-             expected_cs, response[7]);
+             expected_cs, response[6]);
     return false;
   }
   
