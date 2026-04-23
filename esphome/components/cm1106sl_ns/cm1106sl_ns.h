cfd3ee1d5e5f3b169209f263da729bbc4391be1c
rafaflan <rafa.albelda@gmail.com>
2026-04-23 13:16:47 +0200

    fix

--- esphome/components/cm1106sl_ns/cm1106sl_ns.h
+++ esphome/components/cm1106sl_ns/cm1106sl_ns.h
@@ -26,7 +26,7 @@ class CM1106SLNSComponent : public PollingComponent, public uart::UARTDevice {
   uint8_t smoothing_samples_ = 1;        // number of smoothed data points
 
   void cm1106_write_command_(const uint8_t *command, size_t command_len);
-  bool cm1106_serial_read_bytes(uint8_t *response, size_t response_len, uint8_t timeout_seconds);
+  uint8_t cm1106_serial_read_bytes(uint8_t *response, size_t response_len, uint8_t timeout_seconds);
   uint8_t cm1106_checksum_(const uint8_t *response, size_t len);
   bool cm1106_get_working_status_(uint8_t *mode);
   bool cm1106_set_working_status_(uint8_t mode);
